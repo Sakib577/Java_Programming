70a2413fedc21c0a8179f0d2143733168d09232c
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2024-09-08 10:29:32 +0600

    updated

--- Constructor.java
+++ Constructor.java
@@ -1,12 +1,21 @@
 public class Constructor {
     int x;
+    int y;
 
-    public Constructor() {
+    public Constructor() {// constructor
         x = 10;
     }
 
+    public Constructor(int z) {// using parameter in a constructor
+        y = z;
+    }
+
     public static void main(String[] args) {
-        Constructor object1= new Constructor();
+        Constructor object1 = new Constructor();
         System.out.println(object1.x);
+        System.out.println(object1.y); // if I don't use parameter.
+
+        Constructor object2 = new Constructor(20);
+        System.out.println(object2.y);
     }
 }
