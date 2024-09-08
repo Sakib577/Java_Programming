7bae1ee58c1259f5587ffc450794d43c682c99f3
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2024-09-08 10:18:11 +0600

    added

---
+++ Constructor.java
@@ -0,0 +1,12 @@
+public class Constructor {
+    int x;
+
+    public Constructor() {
+        x = 10;
+    }
+
+    public static void main(String[] args) {
+        Constructor object1= new Constructor();
+        System.out.println(object1.x);
+    }
+}
